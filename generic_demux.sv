faff2864cdf46fea639f9cc9ac647488823e2d25
Shashank V M <58113556+ShashankVM@users.noreply.github.com>
2021-03-11 21:22:53 +0530

    Create generic_demux.sv

---
+++ generic_demux.sv
@@ -0,0 +1,10 @@
+module generic_demux #(parameter WIDTH = 1, 
+                   parameter NUMBER = 2, 
+                   parameter SELECT_W = $clog2(NUMBER)) 
+ (input logic [SELECT_W-1:0] sel, 
+  input logic [WIDTH-1:0] mux_in,                   
+  output logic [WIDTH-1:0] out [NUMBER-1:0]);
+  
+  assign out[sel] = mux_in;
+    
+endmodule  
