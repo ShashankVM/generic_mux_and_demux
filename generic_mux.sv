a132848e680779c4f2140785a54dac40e4c4a6a7
Shashank V M <58113556+ShashankVM@users.noreply.github.com>
2021-03-11 21:21:52 +0530

    Update generic_mux.sv

--- generic_mux.sv
+++ generic_mux.sv
@@ -2,7 +2,7 @@ module generic_mux #(parameter WIDTH = 1,
                    parameter NUMBER = 2, 
                    parameter SELECT_W = $clog2(NUMBER)) 
  (input logic [SELECT_W-1:0] sel, 
-  logic [WIDTH-1:0] mux_in [NUMBER-1:0],                   
+  input logic [WIDTH-1:0] mux_in [NUMBER-1:0],                   
   output logic [WIDTH-1:0] out);
   
   assign out = mux_in[sel];
